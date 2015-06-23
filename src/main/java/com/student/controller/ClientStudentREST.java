1124e5592b171085c61ab500cf71fe40b39fad63
fernando.villar <fernandovillar88@gmail.com>
2015-06-23 10:50:07 -0300

    student commit

---
+++ src/main/java/com/student/controller/ClientStudentREST.java
@@ -0,0 +1,74 @@
+package com.student.controller;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+ 
+public class ClientStudentREST {
+ 
+    public static final String SERVER_URI = "http://localhost:8080/rest-service-gradle/";
+    private static final Logger logger = LoggerFactory.getLogger(ClientStudentREST.class);
+    
+     
+    public static void main(String args[]){
+//        testGetDummyStudent();
+//        System.out.println("*****");
+//    	testCreateStudent();
+//        System.out.println("*****");
+//        testGetStudent();
+//        System.out.println("*****");
+        testDeleteStudent();
+        System.out.println("*****");
+//        testGetAllStudents();
+    }
+ 
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void testGetAllStudents() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<LinkedHashMap> students = restTemplate.getForObject(SERVER_URI + StudentConstants.GET_ALL_STUS, List.class);
+        for(LinkedHashMap map : students){
+            System.out.println("ID="+map.get("id")+",Name="+map.get("name")+",CreatedDate="+map.get("createdDate"));;
+        }
+    }
+ 
+    /**
+     * 
+     */
+    public static void testCreateStudent() {
+        RestTemplate restTemplate = new RestTemplate();
+        Student student = new Student();
+        student.setStudentName("Fernando Villar");
+        student.setCourse("AAA");
+        student.setRollNumber(5);
+        Student response = restTemplate.postForObject(SERVER_URI + StudentConstants.CREATE_STUDENT, student, Student.class);
+        printStudentData(response);
+    }
+ 
+    public static void testGetStudent() {
+        RestTemplate restTemplate = new RestTemplate();
+        int id = 1;
+        Student student = restTemplate.getForObject(SERVER_URI + "/rest/student/"+id, Student.class);
+        printStudentData(student);
+    }
+    
+    public static void testDeleteStudent() {
+        RestTemplate restTemplate = new RestTemplate();
+        int id = 1;
+        String student = restTemplate.postForObject(SERVER_URI + "rest/student/delete", id, String.class);
+        System.out.println(student);
+    }
+ 
+    private static void testGetDummyStudent() {
+        RestTemplate restTemplate = new RestTemplate();
+        Student student = restTemplate.getForObject(SERVER_URI + StudentConstants.DUMMY_STUDENT, Student.class);
+        printStudentData(student);
+    }
+     
+    public static void printStudentData(Student student){
+        System.out.println("ID=" + student.getId() + ", Name=" + student.getStudentName());
+    }
+}
